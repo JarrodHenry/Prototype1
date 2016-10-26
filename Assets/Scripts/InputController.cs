83b504bf6210f0f80c0e27d212a1cb1e1c7a85f8
Jarrod Henry <jarrod.henry@sftsrc.com>
2016-10-26 08:54:55 -0700

    Init commit, nandgate testing, and gatemodule with all other gates currently using nand logic

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class InputController : MonoBehaviour {
+
+    private SpriteRenderer sr;
+    [HideInInspector]
+    public bool Status { get; set; }
+	// Use this for initialization
+	void Start () {
+        sr = GetComponent<SpriteRenderer>();
+        Status = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        
+        if (Status)
+        {
+            sr.color = new Color(255, 255, 255);
+            Status = false;
+        }
+        else
+        {
+            sr.color = new Color(255, 0, 0);
+            Status = true;
+        }
+    }
+}
---
+++ Assets/Scripts/NandController.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NandController : MonoBehaviour {
+
+    public Transform inputA;
+    public Transform inputB;
+    public Transform output;
+    private InputController scriptA;
+    private InputController scriptB;
+    
+	// Use this for initialization
+	void Start () {
+        
+        inputA = transform.FindChild("Input A");
+        inputB = transform.FindChild("Input B");
+        
+        output = transform.FindChild("Output");
+      
+         scriptA = inputA.GetComponent<InputController>();
+         scriptB = inputB.GetComponent<InputController>();
+	}
+	
+	
+	void FixedUpdate () {
+
+        bool result = GateModule.NandGate(scriptA.Status, scriptB.Status);
+        // bool result = GateModule.(scriptA.Status);
+        if (result)
+        {
+            output.GetComponent<SpriteRenderer>().color = new Color(255, 0, 0);
+        }
+        else
+        {
+            output.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255);
+        }
+	}
+}
+
+public class GateModule
+{
+    public static bool AndGate(bool a, bool b)
+    {
+        // Programatically
+        //return a && b;
+
+        // NAND Logic
+        bool nand1 = NandGate(a, b);
+        return NandGate(nand1, nand1);
+
+    }
+
+    public static bool OrGate(bool a, bool b)
+    {
+        //Programatically
+        //return a || b;
+
+        // NAND Logic
+        bool nand1 = NandGate(a, a);
+        bool nand2 = NandGate(b, b);
+        return NandGate(nand1, nand2);
+    }
+
+    public static bool NotGate(bool a)
+    {
+        // return !a;
+        return NandGate(a, a);
+    }
+
+    public static bool XorGate(bool a, bool b)
+    {
+        // Programmatically 
+        // return a ^ b;
+
+        // NAND Logic
+        bool nand1 = NandGate(a, b);
+        bool nand2 = NandGate(nand1, a);
+        bool nand3 = NandGate(nand1, b);
+        return NandGate(nand2, nand3);
+
+    }
+
+    public static bool NandGate(bool a, bool b)
+    {
+        return !(a && b);
+    }
+
+    public static bool NorGate(bool a, bool b)
+    {
+        return !(a || b);
+    }
+}
